99d0f4714f0ad27dfad88946c1a6ead7a4cfeb8b
Stefan Mandaric <stefan.mandaric@omnysecurity.com>
2024-02-03 23:29:02 +0100

    4.8 Seeding contacts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,10 +1,19 @@
-import { clearSupabaseData, createUser, startSupabase } from "./utils";
+import { clearSupabaseData, createUser, startSupabase, createContact } from "./utils";
 
 async function seed() {
 	try {
 		await startSupabase();
 		await clearSupabaseData();
-		await createUser({ email: "t@t.com", full_name: "Test user", password: "password" });
+		const user = await createUser({
+			email: "t@t.com",
+			full_name: "Test user",
+			password: "password"
+		});
+
+		for (let i = 0; i < 5; i++) {
+			await createContact(user.id);
+		}
+
 		console.log("Seeded sucessfully");
 	} catch (err) {
 		console.error(err);
