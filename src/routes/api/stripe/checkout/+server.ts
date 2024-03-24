b2a606e30602ff09d102252cb31f6168cbdb9ef2
Stefan Mandaric <stefan.mandaric@omnysecurity.com>
2024-03-24 23:25:43 +0100

    10.4 Prevent multiple plans

--- src/routes/api/stripe/checkout/+server.ts
+++ src/routes/api/stripe/checkout/+server.ts
@@ -1,11 +1,17 @@
 import { error, redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
-import { createCheckoutSession } from "$lib/server/subscriptions";
+import { createCheckoutSession, getSubscriptionTier } from "$lib/server/subscriptions";
 
 export const GET: RequestHandler = async (event) => {
 	const session = await event.locals.getSession();
 	if (!session) {
 		throw redirect(302, "/login");
+	}
+
+	const tier = await getSubscriptionTier(session.user.id);
+
+	if (tier !== "Free") {
+		throw redirect(302, "/account/billing");
 	}
 
 	const price_id = event.url.searchParams.get("id");
