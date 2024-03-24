c33b23e81f9640ff9f7e0aa3d68d0bb9a2ff7937
Stefan Mandaric <stefan.mandaric@omnysecurity.com>
2024-03-24 22:44:45 +0100

    9.8 Deliver customer portal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -56,7 +56,7 @@
 						<span class="block truncate text-sm font-medium"> {session.user.email} </span>
 					</div>
 					<DropdownItem href="/account">Settings</DropdownItem>
-					<DropdownItem href="/account">Billing</DropdownItem>
+					<DropdownItem href="/account/billing">Billing</DropdownItem>
 					<form action="/logout" method="POST">
 						<DropdownItem type="submit" slot="footer">Sign out</DropdownItem>
 					</form>
